3238116f8d534575616b5786d8830296dbcd7360
Nitravee Wongvitavas <nitravee@Nitravees-MacBook-Pro.local>
2018-10-04 18:27:34 +0700

    Reconciled data

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 // App code here
+
+const fs = require('fs');
+const glob = require('glob');
+const path = require('path');
+
+function ndid() {
+  const tokenReport = glob.sync('test-data/GetUsedTokenReport/*.json');
+  const requests = [];
+  tokenReport.forEach((file) => {
+    const filename = path.basename(file).split('.')[0];
+    const data = JSON.parse(fs.readFileSync(file));
+    data.forEach((request) => {
+      if (request.hasOwnProperty('request_id')) {
+        request.nodeId = filename;
+        requests.push(request);
+      }
+    });
+  });
+
+  const requestDetail = glob.sync('test-data/RequestDetail/*.json');
+  const details = [];
+  requestDetail.forEach((file) => {
+    details.push(JSON.parse(fs.readFileSync(file)));
+  });
+
+  const data = {};
+  details.forEach((detail) => {
+    data[detail.request_id] = {};
+    data[detail.request_id].detail = detail;
+    const steps = [];
+    requests.forEach((request) => {
+      const step = {};
+      if (request.request_id === detail.request_id) {
+        step.height = request.height;
+        step.method = request.method;
+        step.nodeId = request.nodeId;
+        steps.push(step);
+      }
+    });
+    data[detail.request_id].steps = steps;
+  });
+
+  return data;
+}
+
+module.exports.ndid = ndid;
--- test/app.test.js
+++ test/app.test.js
@@ -1,15 +1,21 @@
 const chai = require('chai');
 
 const { expect } = chai;
+const { ndid } = require('../app.js');
+
+const _ = require('lodash');
+const fs = require('fs');
 
 // Start test cases for Example Behaviors
 function itAlwaysTrue() {
-  // Chai API Reference can be found at http://www.chaijs.com/api/bdd/
-  expect(true).to.be.true;
+  const data = fs.readFileSync('expected.json');
+  const output = JSON.parse(data);
+  expect(_.isEqual(ndid(), output)).to.be.true;
 }
 
 // Start Example Behaviors
 function exampleBehaviors() {
+  it('should be true', itAlwaysTrue);
 }
 
 // Start Describe
