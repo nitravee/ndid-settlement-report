2b8227ae85293482e8a8255213dbe752fa2dc21d
wichittar ongsakpornnimit <wichittarongsakpornnimit@paimacpro.wifi.bkk.throughwave.com>
2018-10-09 14:06:15 +0700

    Add File createSummaryReport and categorizedRequests

---
+++ categorizedRequests.js
@@ -0,0 +1,135 @@
+const checkConditionDetail = {
+    statusAcceptGreaterThanOrEqualMinIdp: (responseList, minIdp) => {
+        let count = 0;
+        responseList.forEach(data => {
+            if (data.status === 'accept') count++
+        })
+        return count >= minIdp
+    },
+    statusRejectGreaterThanMinOne: responseList => {
+        let count = 0;
+        responseList.forEach(data => {
+            if (data.status === 'reject') count++
+        })
+        return count > 1
+    },
+    receivedDataGreaterThanOrEqualMinAs: dataRequestList => {
+        let check = [];
+        dataRequestList.forEach((data, index) => {
+            check[index] = data.received_data_from_list.length >= data.min_as
+
+        })
+        return check.every(booleanInCheck => booleanInCheck)
+    },
+    receivedIsEmptyArray: dataRequestList => {
+        let check = [];
+        dataRequestList.forEach((data, index) => {
+            check[index] = data.received_data_from_list.length === 0
+        })
+        return check.every(booleanInCheck => booleanInCheck)
+    },
+    answeredIsEmptyArray: dataRequestList => {
+        let check = [];
+        dataRequestList.forEach((data, index) => {
+            check[index] = data.answered_as_id_list.length === 0
+        })
+        return check.every(booleanInCheck => booleanInCheck)
+    },
+    fieldValidFalseGreaterThanOneField: responseList => {
+        let count = 0;
+        responseList.forEach(data => {
+            for (let rootName in data) {
+                if (rootName.includes('valid_')) {
+                    if (!data[rootName]) {
+                        count++
+                    }
+                }
+            }
+        })
+        return count > 1
+    },
+    responseListIsEmptyArray: responseList => {
+        return responseList === null
+    },
+    noItemInAnswerAndReceived: dataRequestList => {
+        let check = false
+        dataRequestList.as_id_list.forEach(data => {
+            if(!dataRequestList.received_data_from_list.includes(data)&&!dataRequestList.answered_as_id_list.includes(data)) check=true;
+        })
+        return check;
+    },
+    haveItemInAnswerButNoHaveInReceived: dataRequestList => {
+        let check = false
+        dataRequestList.answered_as_id_list.forEach(data => {
+            if(!dataRequestList.received_data_from_list.includes(data)) check=true;
+        })
+        return check;
+    },
+}
+
+function categorizedRequests(objData) {
+    let settlement = {};
+    let idpList = [];
+    let asList = [];
+    let requester_node_id = 0;
+    for (let rootName in objData) {
+      const { request_id, timed_out, closed, response_list, min_idp, data_request_list } = objData[rootName].detail
+      const { steps, detail } = objData[rootName]
+      let idp_id = 0
+      let as_id = 0
+      steps.forEach(dataInSteps => {
+        if (dataInSteps.method === 'CreateRequest') {
+          height = dataInSteps.height
+          requester_node_id = dataInSteps.nodeId
+        } else if (dataInSteps.method === 'CreateIdpResponse') { 
+            let idp_fee_ratio = 1;
+          if (detail.response_list !== null) {
+            detail.response_list.forEach(dataInResponse => {
+              if (dataInResponse.idp_id === dataInSteps.nodeId) {
+                const { status, ial, aal, idp_id } = dataInResponse
+                idpList.push({idp_id, status, ial, aal, idp_fee_ratio})
+              }
+            })
+          } else {
+            idpList.push({idp_id, status: "Not Answer", ial:detail.min_ial, aal:detail.min_aal, idp_fee_ratio})
+          }
+        } else if (dataInSteps.method === 'SignData') {
+          let as_price = 1
+          let service_id = ''
+          as_id = dataInSteps.nodeId
+          if (detail.data_request_list !== null) {
+            detail.data_request_list.forEach(data => {
+              if (data.as_id_list.includes(dataInSteps.nodeId)) {
+                service_id = data.service_id
+                if (data.answered_as_id_list.length !== 0) {
+                  if (!data.answered_as_id_list.includes(dataInSteps.nodeId) && !data.received_data_from_list.includes(dataInSteps.nodeId) && detail.closed) as_price = 0 //เคสที่asไม่ได้เงิน
+                }
+              }
+            })
+            if (as_price > 0) asList.push({
+              as_id,
+              service_id
+            })
+          }
+        } else if (dataInSteps.method === 'TimeOutRequest'){ 
+            const newSteps =  steps.filter(data => data.method === 'CreateIdpResponse')
+            if(detail.idp_list.length > newSteps.length) {
+                if(newSteps.length>=1){
+                     detail.idp_list.forEach(data => {
+                         newSteps.forEach(dataInNewSteps => {
+                             if(dataInNewSteps.nodeId !== data) idpList.push({idp_id:data, status: "Not Answer", ial:detail.min_ial, aal:detail.min_aal, idp_fee_ratio:0.5})
+                         })
+                   
+                     })
+                } else {idpList.push({idp_id:detail.idp_list[0], status: "Not Answer", ial:detail.min_ial, aal:detail.min_aal, idp_fee_ratio:0.5})}
+                 
+            }
+        }
+      })
+      settlement = {request_id, requester_node_id, height, idpList, asList, closed}
+      objData[rootName] = { ...objData[rootName], settlement }
+    }
+    return objData
+  }
+
+module.exports.categorizedRequests = categorizedRequests;
---
+++ createSummaryReport.js
@@ -0,0 +1,264 @@
+
+const fs = require('fs');
+
+const dataTest = {
+    "1de6e10b2933948cdca039081f5eacd02683e74b51db62e55da60d61dca558ba": {
+      "detail": {
+        "request_timeout": 60,
+        "requester_node_id": "ncb_rp_1",
+        "response_list": [
+          {
+            "status": "accept",
+            "ial": 2.3,
+            "private_proof_hash": "",
+            "aal": 2.2,
+            "identity_proof": "",
+            "valid_proof": null,
+            "idp_id": "mock_idp_2",
+            "valid_signature": null,
+            "signature": "this is signature",
+            "valid_ial": null
+          },
+          {
+            "status": "accept",
+            "ial": 2.3,
+            "private_proof_hash": "",
+            "aal": 2.2,
+            "identity_proof": "",
+            "valid_proof": null,
+            "idp_id": "mock_idp_1",
+            "valid_signature": null,
+            "signature": "this is signature",
+            "valid_ial": null
+          }
+        ],
+        "min_idp": 2,
+        "timed_out": false,
+        "min_aal": 2.2,
+        "data_request_list": [
+          {
+            "as_id_list": [
+              "ncb_as_2"
+            ],
+            "answered_as_id_list": [
+              "ncb_as_2"
+            ],
+            "request_params_hash": "XZVncUNW3Tmep5UiPuneZPQ2FTanWPB3E3Mgqy6/sFg=",
+            "received_data_from_list": [
+              "ncb_as_2"
+            ],
+            "service_id": "002.credit_info_001",
+            "min_as": 1
+          }
+        ],
+        "special": false,
+        "closed": true,
+        "request_id": "1de6e10b2933948cdca039081f5eacd02683e74b51db62e55da60d61dca558ba",
+        "min_ial": 2.3,
+        "request_message_hash": "nd//Vrgrr9UYKz/Tj3CQu56TQTmdGX7m2B3GlKomRzw=",
+        "mode": 1
+      },
+      "steps": [
+        {
+          "height": 5748,
+          "method": "CreateIdpResponse",
+          "nodeId": "mock_idp_1"
+        },
+        {
+          "height": 5747,
+          "method": "CreateIdpResponse",
+          "nodeId": "mock_idp_2"
+        },
+        {
+          "height": 5750,
+          "method": "SignData",
+          "nodeId": "ncb_as_2"
+        },
+        {
+          "height": 5745,
+          "method": "CreateRequest",
+          "nodeId": "ncb_rp_1"
+        },
+        {
+          "height": 5752,
+          "method": "SetDataReceived",
+          "nodeId": "ncb_rp_1"
+        },
+        {
+          "height": 5754,
+          "method": "CloseRequest",
+          "nodeId": "ncb_rp_1"
+        }
+      ],
+      "settlement":
+        {
+          "request_id": "1de6e10b2933948cdca039081f5eacd02683e74b51db62e55da60d61dca558ba",
+          "requester_node_id": "ncb_rp_1",
+          "height": 5748,
+          "idpList": [
+            {
+              "idp_id": "mock_idp_1",
+              "status": "accept",
+              "ial": 2.3,
+              "aal": 2.2,
+              "idp_fee_ratio": 1
+            },
+            {
+              "idp_id": "mock_idp_2",
+              "status": "accept",
+              "ial": 2.3,
+              "aal": 2.2,
+              "idp_fee_ratio": 0.5
+            }
+          ],
+          "asList": [
+            {
+              "as_id": "ncb_as_2",
+              "service_id": "002.credit_info_001",
+              "as_fee" : 50
+            }
+          ],
+          "closed": true
+        }
+    },
+    "1de0e10b2933948cdca039081f5eacd02683e74b51db62e55da60d61dca558ba": {
+      "detail": {
+        "request_timeout": 60,
+        "requester_node_id": "ncb_rp_1",
+        "response_list": [
+          {
+            "status": "accept",
+            "ial": 2.3,
+            "private_proof_hash": "",
+            "aal": 2.2,
+            "identity_proof": "",
+            "valid_proof": null,
+            "idp_id": "mock_idp_2",
+            "valid_signature": null,
+            "signature": "this is signature",
+            "valid_ial": null
+          },
+          {
+            "status": "accept",
+            "ial": 2.3,
+            "private_proof_hash": "",
+            "aal": 2.2,
+            "identity_proof": "",
+            "valid_proof": null,
+            "idp_id": "mock_idp_1",
+            "valid_signature": null,
+            "signature": "this is signature",
+            "valid_ial": null
+          }
+        ],
+        "min_idp": 2,
+        "timed_out": false,
+        "min_aal": 2.2,
+        "data_request_list": [
+          {
+            "as_id_list": [
+              "ncb_as_2"
+            ],
+            "answered_as_id_list": [
+              "ncb_as_2"
+            ],
+            "request_params_hash": "XZVncUNW3Tmep5UiPuneZPQ2FTanWPB3E3Mgqy6/sFg=",
+            "received_data_from_list": [
+              "ncb_as_2"
+            ],
+            "service_id": "002.credit_info_001",
+            "min_as": 1
+          }
+        ],
+        "special": false,
+        "closed": true,
+        "request_id": "1de6e10b2933948cdca039081f5eacd02683e74b51db62e55da60d61dca558ba",
+        "min_ial": 2.3,
+        "request_message_hash": "nd//Vrgrr9UYKz/Tj3CQu56TQTmdGX7m2B3GlKomRzw=",
+        "mode": 1
+      },
+      "steps": [
+        {
+          "height": 5748,
+          "method": "CreateIdpResponse",
+          "nodeId": "mock_idp_1"
+        },
+        {
+          "height": 5747,
+          "method": "CreateIdpResponse",
+          "nodeId": "mock_idp_2"
+        },
+        {
+          "height": 5750,
+          "method": "SignData",
+          "nodeId": "ncb_as_2"
+        },
+        {
+          "height": 5745,
+          "method": "CreateRequest",
+          "nodeId": "ncb_rp_1"
+        },
+        {
+          "height": 5752,
+          "method": "SetDataReceived",
+          "nodeId": "ncb_rp_1"
+        },
+        {
+          "height": 5754,
+          "method": "CloseRequest",
+          "nodeId": "ncb_rp_1"
+        }
+      ],
+      "settlement":
+        {
+          "request_id": "1de6e10b2933948cdca039081f5eacd02683e74b51db62e55da60d61dca558ba",
+          "requester_node_id": "ncb_rp_1",
+          "height": 5748,
+          "idpList": [
+            {
+              "idp_id": "mock_idp_1",
+              "status": "accept",
+              "ial": 2.3,
+              "aal": 2.2,
+              "idp_fee_ratio": 1
+            },
+            {
+              "idp_id": "mock_idp_2",
+              "status": "accept",
+              "ial": 2.3,
+              "aal": 2.2,
+              "idp_fee_ratio": 0.5
+            }
+          ],
+          "asList": [
+            {
+              "as_id": "ncb_as_2",
+              "service_id": "002.credit_info_001"
+            }
+          ],
+          "closed": true
+        }
+    }
+}
+function createSummaryReport(objData) {
+    let idp_price = 0;
+    let idp_full_price = 0;
+    let as_price = 0;
+  const data = JSON.parse(fs.readFileSync('expectedPriceList.json'));
+  for(let rootName in objData) {
+    const priceList = data
+        .filter(item => objData[rootName].settlement.height >= item['min_block_height'] && objData[rootName].settlement.height <= item['max_block_height'])[0]['prices'];
+     objData[rootName].settlement.idpList.forEach((dataInIdpList,index) => {
+         const {aal, ial, idp_id,} = dataInIdpList
+         idp_full_price = priceList['idp'][idp_id][aal][ial];
+         idp_price = (idp_full_price)*(dataInIdpList.idp_fee_ratio)
+         objData[rootName].settlement.idpList[index] = {...dataInIdpList,idp_price,idp_full_price}
+     });
+     objData[rootName].settlement.asList.forEach((dataInAsList,index) => {  
+       const {as_id, service_id} = dataInAsList  
+      as_price = priceList['as'][as_id][service_id];
+      objData[rootName].settlement.asList[index] = {...dataInAsList,as_price}
+  });
+  }
+}
+createSummaryReport(dataTest);
