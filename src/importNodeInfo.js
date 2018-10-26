a2a1a50e04cade37570767f55c4a3c4279164961
Prapat Borriboonpoka <p.borriboonpoka@gmail.com>
2018-10-26 14:19:52 +0700

    Derive node_name_obj of node info from node_name string

--- src/importNodeInfo.js
+++ src/importNodeInfo.js
@@ -8,6 +8,9 @@ function importNodeInfo(nodeInfoDirPath) {
   filePaths.forEach((fpath) => {
     const nodeId = path.basename(fpath, '.json');
     info[nodeId] = JSON.parse(fs.readFileSync(fpath));
+    if (info[nodeId].node_name) {
+      info[nodeId].node_name_obj = JSON.parse(info[nodeId].node_name);
+    }
   });
 
   return info;
