200e2a6156ad82d67e3c68de3432a6648fb13b00
Prapat Borriboonpoka <p.borriboonpoka@gmail.com>
2019-09-05 02:30:49 +0700

    Fix should include prepaid price of NDID fee despite of 0 transaction

--- src/calculateNdidPrice.js
+++ src/calculateNdidPrice.js
@@ -6,27 +6,23 @@ function calculateNdidPrice(planDetail, numberOfStamps) {
   let remainStamps = numberOfStamps;
   let price = 0;
   for (const step of steps) {
-    if (remainStamps <= 0) {
-      break;
-    }
-
     const { type, numberOfStamps: stepStamps } = step;
     const isUnlimited = stepStamps <= 0;
-    let stampCount = remainStamps <= stepStamps ? remainStamps : stepStamps;
-    if (isUnlimited) {
-      stampCount = remainStamps;
-    }
 
-    switch (type) {
-      case RP_PLAN_TYPE.PER_STAMP:
-        price += step.perStampRate * stampCount;
+    if (type === RP_PLAN_TYPE.PER_STAMP) {
+      if (remainStamps <= 0) {
         break;
-      case RP_PLAN_TYPE.PREPAID:
-        price += step.price;
-        break;
+      }
 
-      default:
-        throw new Error(`Unsupported RP plan type - ${type}`);
+      let stampCount = remainStamps <= stepStamps ? remainStamps : stepStamps;
+      if (isUnlimited) {
+        stampCount = remainStamps;
+      }
+      price += step.perStampRate * stampCount;
+    } else if (type === RP_PLAN_TYPE.PREPAID) {
+      price += step.price;
+    } else {
+      throw new Error(`Unsupported RP plan type - ${type}`);
     }
 
     remainStamps = isUnlimited ? 0 : (remainStamps - stepStamps);
